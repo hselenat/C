7c0746c3da373c4ef79789470c75fc35e5d3e8c9
hselenat <942430793@qq.com>
2018-11-15 15:32:45 +0800

    C-one

---
+++ Demo20181115/Demo20181115/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Demo20181115
+//
+//  Created by yh-imac on 2018/11/15.
+//  Copyright © 2018 hselenat. All rights reserved.
+//
+
+#include <stdio.h>//编译预处理指令【standard input & output】
+
+int main(int argc, const char * argv[])//定义主函数,函数的类型为int整型
+{//函数开始标志
+    // insert code here...
+    printf("This is a C program.\n");//输出所指定的一行信息
+    return 0;//函数执行完毕返回函数值0【当主函数正常结束时，f得到函数值为o0，出现异常时出现非0的整数】
+}//函数结束的标志
